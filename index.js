b84208650e21e9f1fe6d2356a7ec91d8448684ee
Tom Byers <tombaromba@gmail.com>
2015-10-30 12:27:08 +0000

    Separate parts out into components

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-var yaml = require('js-yaml');
+(function (window) {
+  var yaml = require('js-yaml'),
+      pageURL = require('./lib/url.js');
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var str = '"key": value',
-      result;
-
-  try {
-    result = yaml.safeLoad(str);
-    console.log(result);
+  function renderStatus(statusText) {
+    document.getElementById('status').textContent = statusText;
   }
-  catch (e) {
-    console.log('bah, YAML error!');
-  }
-});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var callback = function (urlStr) {
+      var str = "key: >\n  'it's a thing'",
+          url = pageURL(urlStr),
+          result;
+      
+      console.log('url');
+      console.log(url);
+      renderStatus('Validating...');
+      try {
+        result = yaml.safeLoad(str, {
+          'onWarning': function () {
+            console.log(arguments);
+          }
+        });
+        renderStatus(result);
+        console.log(result);
+      }
+      catch (e) {
+        console.log('bah, YAML error!');
+        console.log(e);
+      }
+    };
+
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabs) {
+      var tab = tabs[0];
+      var url = tab.url;
+
+      callback(url);
+    });
+  });
+})(window);
